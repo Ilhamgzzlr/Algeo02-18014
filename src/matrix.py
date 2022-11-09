ad61524400d1c6b753d24b545f68bf0e768f5b42
Ilhamgzzlr <ilhamakbar21feb@gmail.com>
2022-11-09 14:54:17 +0700

    matrix.py

---
+++ src/matrix.py
@@ -0,0 +1,79 @@
+#   Membuat dan Menampilkan Matriks
+
+def buatMatrix (Rows, Cols):
+    mat = [[0 for j in range(Cols)] for i in range(Rows)]
+    return (mat)
+
+def displayMatrix (mat):
+    row = len(mat)
+    col = len(mat[0])
+    
+    for i in range(0, row, 1):
+        for j in range(0, col, 1):
+            if (j == col-1):
+                print(mat[i][j])
+            else:
+                print(mat[i][j] , end=' ')
+    print("\n")
+
+#   Membuat dan Menampilkan List Matriks
+def buatListMatrix (N, Rows, Cols):
+    l = [[[0 for j in range(Cols)] for i in range(Rows)] for k in range(N)]
+    return (l)
+
+def displayListMatrix (l):
+    N = len(l)
+    for k in range(0, N, 1):
+        displayMatrix(l[k])
+
+#   Operasi Terhadap Matriks
+def tambahMatrix (mat1, mat2):
+    row = len(mat1)
+    col = len(mat1[0])
+    ret = buatMatrix(row, col)
+    
+    for i in range(0, row, 1):
+       for j in range(0, col, 1):
+           ret[i][j] = mat1[i][j] + mat2[i][j]
+    return (ret)
+
+def kurangMatrix (mat1, mat2):
+    row = len(mat1)
+    col = len(mat1[0])
+    ret = buatMatrix(row, col)
+    
+    for i in range(0, row, 1):
+       for j in range(0, col, 1):
+           ret[i][j] = mat1[i][j] - mat2[i][j]
+    return (ret)
+
+def kaliMatrix (mat1, mat2):
+    row = len(mat1)
+    col = len(mat2[0])
+    ret = buatMatrix(row, col)
+    
+    for i in range(0, row, 1):
+        for j in range(0, col, 1):
+            for k in range(0, len(mat1[0]), 1):
+                ret[i][j] = ret[i][j] + (mat1[i][k] * mat2[k][j])
+    return (ret)
+
+def matrixBagi (mat, k):
+    row = len(mat)
+    col = len(mat[0])
+    ret = buatMatrix(row, col)
+    
+    for i in range(0, row, 1):
+       for j in range(0, col, 1):
+           ret[i][j] = mat[i][j]/k
+    return (ret)
+
+def transposeMatrix (mat):
+    row = len(mat)
+    col = len(mat[0])
+    ret = buatMatrix(row, col)
+    
+    for i in range(0, row, 1):
+       for j in range(0, col, 1):
+           ret[i][j] = mat[j][i]
+    return (ret)
