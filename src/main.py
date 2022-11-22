592eb4914ab46e976c36c873b1d4e8786aaa8156
Ilhamgzzlr <111135168+Ilhamgzzlr@users.noreply.github.com>
2022-11-22 08:41:34 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -68,3 +68,27 @@ tes = '.\dataset\Alexandra Daddario.jpg'
 TestFace = np.array(load_images_file(tes))
 selisihface = np.array(abs(TestFace - mean))
 print(S)
+
+
+
+S = np.array(load_images_folder(".\dataset"))
+mean = np.mean(S, axis=0)  # Calculate mean of images
+sel = np.array(abs(S - mean))  # Calculate difference between images and mean
+trans = np.transpose(sel)
+cov = np.array(np.matmul(sel, trans))  # Calculate covariance of difference
+# Calculate eigen vector of covariance
+vektoreigen = np.array(vektor_eigen(cov))
+vektoreigen = np.dot(vektoreigen, sel)
+eigenfaces = np.array(np.dot(sel, vektoreigen))  # Calculate eigen face
+# TEST(Inputan)
+tes = '.\dataset\Alexandra Daddario.jpg'
+TestFace = np.array(load_images_file(tes))
+selisihface = np.array(abs(TestFace - mean))
+print(S)
+# Euclidean Distance
+database = np.array(load_images_folder(".\dataset"))
+minimumdiff = database[0]
+for image in database:
+    currentdiff = nilai_eigen(TestFace) - nilai_eigen(image)
+    if currentdiff <= minimumdiff:
+        minimumdiff = currentdiff
